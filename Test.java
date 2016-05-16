eac7d6e52620fa1f42ca335f06b0381365fa4a5f
Arjit Srivastava <Arjit Srivastava>
2016-05-16 21:48:28 +0530

    new file test is added

---
+++ Test.java
@@ -0,0 +1,32 @@
+package acadgild;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int [] numbers={10,20,30,40,50};
+		int i;
+		for(i=0;i<=4;i++)
+			
+		{
+			System.out.println(numbers[i]);
+		
+			String [] names={"Arjit","Tarun","Vivek","Anshu","Rohit"};
+			int j;
+			for(j=0;j<=4;j++)
+			{
+				
+				System.out.println(names[j]);
+			}
+			
+		}
+		
+
+	
+	
+	
+	}
+	
+	
+	
+
+}
